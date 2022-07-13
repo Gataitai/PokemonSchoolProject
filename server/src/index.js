3891fe24b6b0f77cdc9023514bccb7c96097263a
Richard Bevers <r.a.bevers@saxion.nl>
2022-07-13 15:49:25 +0200

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const auth = require('./routes/auth');
+
+app.use("/auth", auth);
+
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
