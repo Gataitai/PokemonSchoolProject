a6996da2ea98d283667907007e00cc4db8bda561
Gataitai <mpattipeilohy1@gmail.com>
2022-09-20 14:50:45 +0200

    wip project

--- server/src/data/pokemonData.js
+++ server/src/data/pokemonData.js
@@ -1,49 +1,55 @@
-const pokemonData = [
+const pokemon = [
+    id,
+    name,
+    type
+]
+
+const data = [
     {
-        "id": "1",
-        "name": "Bulbasaur",
-        "type": "grass"
+        id: "1",
+        name: "Bulbasaur",
+        type: "grass"
     },
     {
-        "id": "2",
-        "name": "Ivysaur",
-        "type": "grass"
+        id: "2",
+        name: "Ivysaur",
+        type: "grass"
     },
     {
-        "id": "3",
-        "name": "Venusaur",
-        "type": "grass"
+        id: "3",
+        name: "Venusaur",
+        type: "grass"
     },
     {
-        "id": "4",
-        "name": "Charmander",
-        "type": "fire"
+        id: "4",
+        name: "Charmander",
+        type: "fire"
     },
     {
-        "id": "5",
-        "name": "Chameleon",
-        "type": "fire"
+        id: "5",
+        name: "Chameleon",
+        type: "fire"
     },
     {
-        "id": "6",
-        "name": "Charizard",
-        "type": "fire"
+        id: "6",
+        name: "Charizard",
+        type: "fire"
     },
     {
-        "id": "7",
-        "name": "Squirtle",
-        "type": "water"
+        id: "7",
+        name: "Squirtle",
+        type: "water"
     },
     {
-        "id": "8",
-        "name": "Wartortle",
-        "type": "water"
+        id: "8",
+        name: "Wartortle",
+        type: "water"
     },
     {
-        "id": "9",
-        "name": "Blastoise",
-        "type": "water"
+        id: "9",
+        name: "Blastoise",
+        type: "water"
     }
 ];
 
-module.exports = pokemonData;
+module.exports = {pokemon, data};
--- server/src/routes/pokemon.js
+++ server/src/routes/pokemon.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const pokemonService = require("../services/pokemonService");
+const pokemonData = require("../data/pokemonData")
 
 router.get("/", async (req, res) => {
     const pokemons = pokemonService.getAll();
     res.json({
         pokemons
+    })
+});
+
+router.get("/", async (req, res) => {
+    const pokemons = pokemonService.getAll();
+    res.json({
+        pokemons
+    })
+});
+
+router.post("/{id}", async (req, res) => {
+    const pokemon = req.body.filter(key => pokemonData.pokemon.includes(key));
+
+    console.log(pokemon);
+    res.json({
+        pokemon
     })
 });
 
