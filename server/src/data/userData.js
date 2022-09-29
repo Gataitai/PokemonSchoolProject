41943ad0552042ecdbaf10477cc60653e79c78aa
mpattipeilohy <512391@student.saxion.nl>
2022-09-29 22:48:38 +0200

    wip project

--- server/src/data/userData.js
+++ server/src/data/userData.js
@@ -1,9 +1,26 @@
-const user = [
-    username,
-    password,
-    email,
-    type
-]
+const Joi = require('joi');
+
+const user = Joi.object({
+    username: Joi.string()
+        .alphanum()
+        .max(30)
+        .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+    email: Joi.string()
+        .email(),
+
+    type: Joi.object()
+})
+
+// const user = [
+//     username,
+//     password,
+//     email,
+//     type
+// ]
 
 const data = [];
 
