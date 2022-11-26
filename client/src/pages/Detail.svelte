c074d245bb79e065b3168a706c73ed44c13e4f5e
mpattipeilohy <512391@student.saxion.nl>
2022-11-26 17:27:20 +0100

    wip project

--- client/src/pages/Detail.svelte
+++ client/src/pages/Detail.svelte
@@ -1,25 +1,35 @@
 <script>
     import {getById} from "../util/fetch";
     import Card from "../components/card/PokemonCard.svelte";
+    import {imgUrlId} from "../util/images";
+    import TypeBadgeList from "../components/badges/TypeBadgeList.svelte";
 
     export let id;
     let promise = getById({
         resource: "auctions",
         id: id
     });
-
 </script>
 
 {#await promise}
     Loading
-{:then pokemon}
+{:then auction}
     <div class="container">
         <div class="images">
             {id}
         </div>
 
         <div class="detail">
-            <Card pokemon={pokemon}/>
+
+            <img class="{auction.pokemon.typeList[0].toLowerCase() + '-pastel'}" src={imgUrlId(auction.pokemon.id)} alt="pokemon">
+            <div class="detail-about">
+                <h1>#{auction.pokemon.id} {auction.pokemon.name}</h1>
+                <TypeBadgeList types={auction.pokemon.typeList}/>
+                <span class="detail-time-price">
+                    test
+                </span>
+            </div>
+
         </div>
 
         <div class="bids">
@@ -33,7 +43,6 @@
 <style>
     .container{
         display:flex;
-        height: 100%;
         justify-content: space-between;
         gap: 2rem;
     }
@@ -44,9 +53,39 @@
     }
 
     .detail{
-        width: 100%;
-        background-color: green;
+        width: 150%;
+        display: flex;
     }
+
+    .detail img{
+        border-radius: .5rem 0 0 .5rem;
+        width: 20vw;
+        object-fit: cover;
+    }
+
+    .detail-about{
+        display: flex;
+        flex-direction: column;
+        background-color: var(--bg-primary);
+        border-radius: .5rem;
+        padding: 2rem;
+        width: 100%;
+    }
+
+    .detail-about h1{
+        color: var(--text-primary);
+        margin: 0 0 1rem 0;
+        padding: 0;
+    }
+
+    .detail-time-price{
+        display: flex;
+        margin-top: auto;
+        background-color: var(--bg-secondary);
+        height: 4rem;
+        border-radius: .5rem;
+    }
+
 
     .bids{
         width: 100%;
