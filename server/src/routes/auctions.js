d1791015271001c7bb8f0b17f8bf765dc0aae28b
mpattipeilohy <512391@student.saxion.nl>
2022-12-26 22:53:16 +0100

    wip project

--- server/src/routes/auctions.js
+++ server/src/routes/auctions.js
@@ -5,48 +5,51 @@ const {validateAuction} = require("../middleware/validate");
 const {authorizeToken} = require("../middleware/authorize");
 const {getUserName} = require("../util/getUser");
 
-router.get("/", (req, res) => {
-    const types = req.query.types;
-    const name = req.query.name;
-    const price = req.query.price;
-    const region = req.query.region;
+const parseAuctionQueryParams = (params) => {
+    const searchParams = new URLSearchParams(params);
+    const filters = {};
+    if (searchParams.has('typeList')) filters.typeList = searchParams.get('typeList').split(',');
+    if (searchParams.has('name')) filters.name = searchParams.get('name');
+    if (searchParams.has('startingPrice')) filters.startingPrice = parseInt(searchParams.get('startingPrice'), 10);
+    return filters;
+}
 
-    let auctions;
+router.get("/", async (req, res) => {
+    try {
+        const page = Number(req.query.page) || 1
+        const pageSize = Number(req.query.pageSize) || 10
+        const filters = parseAuctionQueryParams(req.query);
+        const auctions = auctionService.get(filters, page, pageSize)
 
-    if(types){
-        auctions = auctionService.getByTypes(types.split(','));
+        res.json({
+            auctions
+        });
+    } catch (error) {
+        res.status(500).json({error: error.message});
     }
-    else if(name){
-        auctions = auctionService.getByName(name);
-    }
-    else if(price){
-        auctions = auctionService.getByPrice(price)
-    }
-    else if(region){
-        auctions = auctionService.getByRegion(region)
-    }
-    else{
-        auctions = auctionService.getAll();
-    }
-
-    res.json({
-        auctions
-    })
 });
 
-router.get("/:id", (req, res) => {
-    let auction = auctionService.getById(req.params.id);
-    res.json({
-        auction
-    })
+router.get("/:id", async (req, res) => {
+    try {
+        let auction = auctionService.getById(req.params.id);
+        res.json({
+            auction
+        });
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
 });
 
 router.post("/", validateAuction, authorizeToken, (req, res) => {
-    const user = getUserName(req.headers.authorization);
-    const auction = auctionService.save(req.body, user);
-    res.json({
-        auction
-    })
+    try {
+        const user = getUserName(req.headers.authorization);
+        const auction = auctionService.save(req.body, user);
+        res.json({
+            auction
+        });
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
 });
 
 module.exports = router;
--- server/src/services/auctionService.js
+++ server/src/services/auctionService.js
@@ -2,36 +2,77 @@ const auctionData = require("../data/auctionData");
 const crypto = require("crypto");
 const pokemonData = require("../data/pokemonData");
 
-const getAll = () => {
-    return auctionData.data;
+const get = (filters, page, pageSize) => {
+    let auctions = auctionData.data;
+
+    if (filters.typeList) {
+        if(filters.typeList.length >= 2){
+            console.log(filters.typeList)
+            console.log(auctions.filter(a => a.pokemon.typeList[0].includes(filters.typeList) && a.pokemon.typeList[1].includes(filters.typeList)))
+            return auctions.filter(a => a.pokemon.typeList[0].includes(filters.typeList) && a.pokemon.typeList[1].includes(filters.typeList))
+        }
+        else{
+            return auctions.filter(a => a.pokemon.typeList.some(t => filters.typeList.includes(t)));
+        }
+    }
+
+    if (filters.endingDate) {
+        auctions = auctions.filter((auction) => {
+            if (filters.endingDate.startsWith('>=')) {
+                return auction.endingDate >= filters.endingDate.slice(2)
+            } else if (filters.endingDate.startsWith('<=')) {
+                return auction.endingDate <= filters.endingDate.slice(2)
+            }
+        })
+    }
+
+    if (filters.startingPrice) {
+        auctions = auctions.filter((auction) => auction.startingPrice >= filters.startingPrice)
+    }
+
+    if (filters.name) {
+        auctions = auctions.filter((auction) => auction.pokemon.name.toLowerCase().includes(filters.name.toLowerCase()))
+    }
+
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return auctions.slice(startIndex, endIndex);
 }
 
 const getById = (id) => {
     return auctionData.data.find(a => a.id === id);
 }
 
-const getByName = (name) => {
-    return auctionData.data.filter(a => a.pokemon.name.toLowerCase().match(name.toLowerCase()));
-}
-
-const getByTypes = (types) => {
-    console.log(types);
-    if(types.length >= 2){
-        return auctionData.data.filter(a => a.pokemon.typeList[0].includes(types) && a.pokemon.typeList[1].includes(types))
-    }
-    else{
-        return auctionData.data.filter(a => a.pokemon.typeList.some(t => types.includes(t)));
-    }
-}
-
-const getByRegion = (region) => {
-    const generation = pokemonData.generations.find(g => g.name === region);
-    return auctionData.data.filter(p => p.pokemon.id >= generation.from && p.pokemon.id <= generation.to);
-}
-
-const getByPrice = (price) => {
-    return auctionData.data.filter(a => a.startingPrice < price);
-}
+// const getAll = () => {
+//     return auctionData.data;
+// }
+//
+// const getById = (id) => {
+//     return auctionData.data.find(a => a.id === id);
+// }
+//
+// const getByName = (name) => {
+//     return auctionData.data.filter(a => a.pokemon.name.toLowerCase().match(name.toLowerCase()));
+// }
+//
+// const getByTypes = (types) => {
+//     console.log(types);
+//     if(types.length >= 2){
+//         return auctionData.data.filter(a => a.pokemon.typeList[0].includes(types) && a.pokemon.typeList[1].includes(types))
+//     }
+//     else{
+//         return auctionData.data.filter(a => a.pokemon.typeList.some(t => types.includes(t)));
+//     }
+// }
+//
+// const getByRegion = (region) => {
+//     const generation = pokemonData.generations.find(g => g.name === region);
+//     return auctionData.data.filter(p => p.pokemon.id >= generation.from && p.pokemon.id <= generation.to);
+// }
+//
+// const getByPrice = (price) => {
+//     return auctionData.data.filter(a => a.startingPrice < price);
+// }
 
 const save = (auct, user) => {
     const auction = {
@@ -60,13 +101,7 @@ const save = (auct, user) => {
 // }
 
 module.exports = {
-    getAll,
+    get,
     getById,
-    getByName,
-    getByTypes,
-    getByRegion,
-    getByPrice,
     save,
-    // update,
-    // remove,
 };
